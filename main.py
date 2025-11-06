9d7c75c0309967a80216c35a0ec9820cd29b9b21
Lilrosco <rossey.charleston@gmail.com>
2025-11-05 22:46:54 -0500

    Added clock and clock tick as well as cicle shape class.

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     black_background = (0, 0, 0)
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     # Game Loop
     while(1):
@@ -27,6 +29,8 @@ def main():
 
         pygame.Surface.fill(screen, black_background)
         pygame.display.flip() # refresh entire screen
+
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
